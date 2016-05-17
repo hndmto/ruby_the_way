691f7d7315d2cd122632beb6ea3cdd017a5668fa
hanada mitsuo <hanada.mitsuo@adways.net>
2016-05-18 01:11:04 +0900

    add new file

---
+++ hello.rb
@@ -0,0 +1,29 @@
+print "hello world!" #終わった後に改行がない
+puts "hello world(puts)" #改行がある
+p "hello world(p)" #データの形式がわかるように表示
+
+=begin
+aa
+ss
+dd
+ff
+=end
+
+msg = "hello world"
+p msg
+
+ADMIN_EMAIL = "hogehoe@gmail.com"
+#ADMIN_EMAIL = "hogehoge"
+p ADMIN_EMAIL
+
+p "hello world".length
+
+#
+
+x = 10 #110_11
+y = 20.5
+z = 1/3 #Rational(1,3)
+
+p x % 3 #1
+p x ** 3  #1000
+p z * 2 # 2/3
