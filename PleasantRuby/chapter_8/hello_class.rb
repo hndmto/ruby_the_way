68433c16892b13b1db1dbf2b6cd8515523ee787c
花田光夫 <hanada.mitsuo@adways.net>
2016-06-14 22:18:17 +0900

    8章メソッド呼び出しの制限まで

--- PleasantRuby/chapter_8/hello_class.rb
+++ PleasantRuby/chapter_8/hello_class.rb
@@ -3,9 +3,40 @@ class HelloWorld
     @name = myname
   end
 
+  attr_accessor :name2
+
   def hello
     puts "Hello, world. I am #{@name}"
   end
+
+  def name
+    @name
+  end
+
+  def name=(value)
+    @name =value
+  end
+
+  def greet
+    puts "Hi, I am #{self.name2}."
+  end
+
+  def test_name
+    name2 = "Ruby2"
+    self.name2 = "Ruby3"
+  end
+
+#  class << self
+#    def hello(name)
+#      puts "#{name} said hello."
+#    end
+#  end
+
+  def self.hello3(name)
+    puts "#{name} said hello."
+  end
+
+  Version = 1.0
 end
 
 bob = HelloWorld.new("Bob")
@@ -13,3 +44,14 @@ alice = HelloWorld.new("Alice")
 ruby = HelloWorld.new
 
 bob.hello
+
+p bob.name="Robert"
+p bob.name2="Robert"
+bob.greet
+
+test = HelloWorld.new
+test.test_name
+
+HelloWorld.hello3("John")
+
+p HelloWorld::Version
